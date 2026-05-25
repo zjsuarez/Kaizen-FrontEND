32680cc14bd8b9ca56576822cbc802db450037dc
zjsuarez <zjs.suarez@gmail.com>
2026-05-25 17:51:05 +0200

    (feat); #78: Added limit to image uploading in workout body measurements

--- app/src/main/java/com/example/kaizenfrontend/di/NetworkModule.kt
+++ app/src/main/java/com/example/kaizenfrontend/di/NetworkModule.kt
@@ -27,7 +27,7 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
-    private const val BASE_URL = "http://192.168.1.104:8080/" //
+    private const val BASE_URL = "http://10.0.2.2:8080/" //
 
     @Provides
     @Singleton
